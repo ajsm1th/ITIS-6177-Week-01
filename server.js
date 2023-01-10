db58e130aae3200776647e09dea4665e5f2eb3b7
Angie Smith <ajorda1@uncc.edu>
2023-01-09 20:29:17 -0500

    week1-1/9/23

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
+// Importing the http module
+const http = require("http")
+  
+// Creating server 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+    // Sending the response
+    res.write("Hello World")
+    res.end();
+})
+  
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
